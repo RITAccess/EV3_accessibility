9947da6e720f98e279b0e23fc553be8ded673730
Alejandro Olvera <alejandroolvera70@gmail.com>
2017-02-27 19:16:15 -0600

    Updated setup environment

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 opp=$HOME/projects/lms2012/open_first
+echo
+echo "=========================================================================================="
+echo -e " Environment Setup"
+echo "=========================================================================================="
+echo
+sleep 1
 
 echo -e "Setting up pre-reqs\n"
 # Get uboot tools
@@ -14,20 +20,27 @@ echo "Installing ImageMagick"
 sudo apt-get install imagemagick libmagickcore-dev
 echo
 
+sleep 1
 echo "Pushing CodeSourcery to home"
 if [ -d $HOME/CodeSourcery ]; then
 	echo "CodeSourcery already already exists in your home directory."
 else
 	mkdir ~/CodeSourcery
 	ln -s ~/projects/utilities/linuxdevkit/sg++_lite  ~/CodeSourcery/Sourcery_G++_Lite
+	echo "CodeSourcery linked to your home directory."
 fi
+echo
+sleep 1
 
-echo "Pushing am1808 to home"
+echo -e "Pushing am1808 to home"
 if [ -d $HOME/am1808 ]; then
 	echo "am1808 already already exists in your home directory."
 else
 	ln -s ~/projects/utilities/am1808/ ~/am1808
+	echo "Da Vinci SDK linked to your home directory."
 fi
+echo
+sleep 1
 
 echo -e "\nFinished pre-req setup\n"
 echo "Switching directories"
@@ -38,9 +51,7 @@ if [ $opp == $PWD ]; then
 	source env_setup;
 else
 	echo "Unable to switch directories."
+	echo "You may need to run again."
 fi
 
-echo -e "\nAvailable options:"
-make
-
-echo -e "\tDone"
+echo -e "Programs installed, directories made, Done\n"
