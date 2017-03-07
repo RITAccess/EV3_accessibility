b049808534b1b38b862035f385f15b306ec292a0
Alejandro Olvera <alejandroolvera70@gmail.com>
2017-03-07 14:42:30 -0600

    Fixed path issue with mounted SD filesystem (needed to include user)

--- lms2012/open_first/update_sdcard.sh
+++ lms2012/open_first/update_sdcard.sh
@@ -11,36 +11,36 @@ sleep 10
 
 current=${PWD}
 
-if [ -d /media/LMS2012 ]
+if [ -d /media/${USER}/LMS2012 ]
 then
 
-    if [ -d /media/LMS2012_EXT ]
+    if [ -d /media/${USER}/LMS2012_EXT ]
     then
 		# Wipe old data
         echo "  ...."erasing.sdcard
-        sudo rm -r /media/LMS2012/*
-        sudo rm -r /media/LMS2012_EXT/*
+        sudo rm -r /media/${USER}/LMS2012/*
+        sudo rm -r /media/${USER}/LMS2012_EXT/*
         sync
 
 		# Copy new image over
         echo "  ...."copying.kernel.to.sdcard
-        sudo cp uImage /media/LMS2012/uImage
+        sudo cp uImage /media/${USER}/LMS2012/uImage
         sync
 
 		# Copy filesystem over
         echo "  ...."copying.filesystem.to.sdcard
-		sudo cp lmsfs.tar.bz2 /media/LMS2012_EXT
-		cd /media/LMS2012_EXT
+		sudo cp lmsfs.tar.bz2 /media/${USER}/LMS2012_EXT
+		cd /media/${USER}/LMS2012_EXT
 		sudo tar -jxf lmsfs.tar.bz2 
 		sudo rm lmsfs.tar.bz2
 		cd ${current}
         sync
 
         echo "  ...."copying.application.to.sdcard
-        sudo cp -r ~/projects/lms2012/lms2012/Linux_AM1808/* /media/LMS2012_EXT/home/root/lms2012
+        sudo cp -r ~/projects/lms2012/lms2012/Linux_AM1808/* /media/${USER}/LMS2012_EXT/home/root/lms2012
 
         echo "  ...."copying.testprograms.to.sdcard
-        sudo cp -r ~/projects/lms2012/lmssrc/Test/Test /media/LMS2012_EXT/home/root/lms2012/prjs/
+        sudo cp -r ~/projects/lms2012/lmssrc/Test/Test /media/${USER}/LMS2012_EXT/home/root/lms2012/prjs/
 
         echo "  ...."writing.to.sdcard
         sync
