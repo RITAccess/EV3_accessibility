4c67c29073ffb84fdf50ab7ae527098737b7a7da
Alejandro Olvera <alejandroolvera70@gmail.com>
2017-05-27 13:40:10 -0500

    Thought I pushed updated files! Added variables to Ui Instance to provide functionalities for possible scrolling of text on frame update/ after a certain threshold.

--- lms2012/c_ui/source/c_ui.h
+++ lms2012/c_ui/source/c_ui.h
@@ -191,7 +191,8 @@ typedef   struct
 KEYB;
 
 #define   TEXTSIZE      24
-
+#define   SCROLLDELAY   1000
+#define   SCROLLFRAME   6
 typedef   struct
 {
   DATA16  ScreenStartX;
@@ -249,6 +250,14 @@ typedef   struct
   DATA8   FullPath[MAX_FILENAME_SIZE];
   DATA8   Filename[MAX_FILENAME_SIZE];
   DATA8   Text[TEXTSIZE];
+
+
+  DATA16   ScrollOffset;             // The 'counter', utilized to start the text printout at the given start.
+  DATA16   SelectLength;             // The length of the currently selected 
+  DATA8    SelectText[MAX_FILENAME_SIZE];    // The value of the selected text, to determine states of counter.
+  DATA16   DeltaT;                    // Storage that increments continuously, divided by constant (500?) to 
+                                      //    Determing the ScrollOffset.
+  DATA8    Reverse;					  // Reverese increment
 }
 BROWSER;
 
